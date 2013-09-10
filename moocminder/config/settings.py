ddcdca21009f35793fae88d1f6808ab33e838970
Nate Aune <natea@jazkarta.com>
2013-09-10 10:19:12 -0400

    configure the settings file with the scraper settings

--- moocminder/config/settings.py
+++ moocminder/config/settings.py
@@ -370,6 +370,20 @@ LOGGING = {
 
 
 ########## Your stuff: Below this line define 3rd party libary settings
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "moocminder.config.settings") #Changed in DDS v.0.3
+
+BOT_NAME = 'moocscraper'
+
+SPIDER_MODULES = ['dynamic_scraper.spiders', 'moocscraper.scraper',]
+USER_AGENT = '%s/%s' % (BOT_NAME, '1.0')
+
+ITEM_PIPELINES = [
+    'dynamic_scraper.pipelines.ValidationPipeline',
+    'moocscraper.scraper.pipelines.DjangoWriterPipeline',
+]
+
 try:
     from local_settings import *
 except ImportError:

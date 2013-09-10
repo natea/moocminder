4e52c4bbcec1c7258207c8505e96a89b334a4267
Nate Aune <natea@jazkarta.com>
2013-09-09 20:55:18 -0400

    move the local SMTP config into the local_settings file and add a local_settings import

--- moocminder/config/settings.py
+++ moocminder/config/settings.py
@@ -224,12 +224,6 @@ AUTOSLUG_SLUGIFY_FUNCTION = "slugify.slugify"
 if DEBUG:
     # EMAIL_HOST = "localhost"
     # EMAIL_PORT = 1025
-    EMAIL_USE_TLS = True
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = 'nate@noderabbit.com'
-    EMAIL_HOST_PASSWORD = 'changeme'
-    EMAIL_PORT = 587
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
     INSTALLED_APPS += ('debug_toolbar',)
 
@@ -376,3 +370,7 @@ LOGGING = {
 
 
 ########## Your stuff: Below this line define 3rd party libary settings
+try:
+    from local_settings import *
+except ImportError:
+    pass
